89d4ca2bbb3eb0cab337d3efb4dd090d430bb152
Julesntizimira <ntizimijules5@gmail.com>
2024-03-11 23:09:12 +0200

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,23 +5,23 @@ class UsersController {
   static async postNew(req, res) {
     const { body } = req;
     if (!body.email) {
-      res.status(400).send({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
       return;
     }
     if (!body.password) {
-      res.status(400).send({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
       return;
     }
     const client = await dbClient.client;
     const user = await client.db().collection('users').findOne({ email: body.email });
     if (user) {
-      res.status(400).send({ error: 'Already exist' });
+      res.status(400).json({ error: 'Already exist' });
       return;
     }
     const hashedPassword = sha1(body.password);
     const result = await client.db().collection('users')
       .insertOne({ email: body.email, password: hashedPassword });
-    res.status(201).send({ _id: result.insertedId, email: body.email });
+    res.status(201).json({ _id: result.insertedId, email: body.email });
   }
 }
 

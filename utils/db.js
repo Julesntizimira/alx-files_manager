f4e22a66da154e7fcdbc524eaa4c90f08def1c98
Julesntizimira <ntizimijules5@gmail.com>
2024-03-11 18:56:41 +0200

    create mongo db client

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(url);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    const users = await this.client.db().collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    const files = await this.client.db().collection('files').countDocuments();
+    return files;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;

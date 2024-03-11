26b9deda00158bf7bbf079fb8b98b597328620a2
Julesntizimira <ntizimijules5@gmail.com>
2024-03-11 22:35:26 +0200

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,25 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { body } = req;
+    if (!body.email) {
+      return res.status(400).send('Missing email');
+    }
+    if (!body.password) {
+      return res.status(400).send('Missing password');
+    }
+    const client = await dbClient.client;
+    const user = await client.db().collection('users').findOne({ email: body.email });
+    if (user) {
+      return res.status(400).send('Already exist');
+    }
+    const hashedPassword = sha1(body.password);
+    const result = await client.db().collection('users')
+      .insertOne({ email: body.email, password: hashedPassword });
+    return res.status(201).send({ _id: result.insertedId, email: body.email });
+  }
+}
+
+module.exports = UsersController;
